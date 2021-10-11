75e27e20a2daa376db70bf74044b7607615a2872
ichisemasashi <ichise.masashi@gmail.com>
2021-10-11 22:56:11 +0900

    add channel-directions.go

---
+++ go-by-example/channel-directions.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/*
+   この ping 関数は、チャネルを送信専用で受け取ります。 このチャネルで
+   受信しようとすると、コンパイルエラーになります。
+*/
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+/*
+   この pong 関数は、1 つ目のチャネルを受信専用で (pings)、
+   2つ目のチャネルを送信専用で (pongs) 受け取ります。
+*/
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings
+	pongs <- msg
+}
+
+func main() {
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
+}
