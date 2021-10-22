78d9e57e505495b4f622fc3cb02b54486d1809fa
ichisemasashi <ichise.masashi@gmail.com>
2021-10-22 16:17:49 +0900

    add sorting.go

---
+++ go-by-example/sorting.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	// ソートメソッドは、組み込み型ごとに特有です。 例えば、文字列に対する
+	// 例は次の通りです。 ソートはインプレースで行われる、すなわち、
+	// 与えられたスライスを直接変更し、新しくは返さない点に注意してください。
+	strs := []string{"c", "a", "b"}
+	sort.Strings(strs)
+	fmt.Println("Strings:", strs)
+
+	// int をソートする例は次の通りです。
+	ints := []int{7, 2, 4}
+	sort.Ints(ints)
+	fmt.Println("Ints:   ", ints)
+
+	// スライスがすでにソートされているかを確認するために
+	// sort パッケージを使うこともできます。
+	s := sort.IntsAreSorted(ints)
+	fmt.Println("Sorted: ", s)
+}
