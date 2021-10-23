1365c7f354040fba5774eb77866fc104dedd3647
ichisemasashi <ichise.masashi@gmail.com>
2021-10-23 22:28:12 +0900

    for latex files

--- go-by-example/hello-world.go
+++ go-by-example/hello-world.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 /*
-   最初のプログラムは古典的な “hello world” メッセージを表示します。ソースコードは次の通りです。
+   最初のプログラムは古典的な “hello world” メッセージを表示します。
+   ソースコードは次の通りです。
 */
 func main() {
 	fmt.Println("hello world")
