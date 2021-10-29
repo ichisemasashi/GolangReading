23ed585a00adb1f748b1548f0476a8c96b815268
ichisemasashi <ichise.masashi@gmail.com>
2021-10-29 15:03:06 +0900

    add arrays.go

---
+++ go-by-example/arrays.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	// ここでは、ちょうど 5 つの int をもつ配列 a を
+	// 作成しています。要素の型と長さはいずれも配列の型
+	// の一部です。デフォルトでは、配列はゼロ値で初期
+	// 化されるので、 int の場合は 0 になります。
+	var a [5]int
+	fmt.Println("emp:", a)
+
+	// array[index] = value で指定位置に値を設定し、
+	// array[index] によって値を取得できます。
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	// len ビルトイン関数は配列の長さを返します。
+	fmt.Println("len:", len(a))
+
+	// 配列を 1 行で宣言かつ初期化するには、
+	// 次の記法を使います。
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+
+	// 配列型は 1 次元ですが、多次元のデータ構造を
+	// 表す型を構成することもできます。
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d:", twoD)
+}
