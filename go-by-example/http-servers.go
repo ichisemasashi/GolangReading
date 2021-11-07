422e3925c0312854184a81b0d94241d00e5cccd4
ichisemasashi <ichise.masashi@gmail.com>
2021-11-07 15:53:55 +0900

    add http-servers.go

---
+++ go-by-example/http-servers.go
@@ -0,0 +1,44 @@
+// net/http パッケージを使えば、基本的な
+// HTTP サーバーを 書くのは簡単です。
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// net/http サーバーの基本コンセプトは、ハンドラです。
+// ハンドラは、http.Handler インターフェースを実装した
+// オブジェクトです。ハンドラを書く一般的な方法は、適切な
+// シグネチャをもつ関数に対して、http.HandlerFunc アダプタ
+// を使うことです。
+func hello(w http.ResponseWriter, req *http.Request) {
+	// ハンドラとして動作する関数は、引数として
+	// http.ResponseWriter と http.Request を受け取ります。
+	// ResponseWriter は、HTTP レスポンスを書き込むために使われ
+	// ます。ここでは単に “hello\n” とレスポンスします。
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	// このハンドラはもう少しだけ高度です。すべてのHTTPリクエスト
+	// ヘッダを読み込み、それらをレスポンスボディにエコーバックします。
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+	// 便利関数 http.HandleFunc を使ってハンドラをサーバーの
+	// ルーティングに登録します。これは、net/http パッケージ内
+	// の デフォルトルーター を設定し、関数を引数に取ります。
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	// 最後に、ポート番号とハンドラを指定して ListenAndServe
+	// を呼び出します。nil は、上で設定したデフォルトルーターを
+	// 使用することを意味します。
+	http.ListenAndServe(":8090", nil)
+}
