9375c3c18e4c9d64ab263a2939a0b8a665071597
ichisemasashi <ichise.masashi@gmail.com>
2021-11-08 23:07:39 +0900

    add functions.go

--- go-by-example/functions.go
+++ go-by-example/functions.go
@@ -2,15 +2,22 @@ package main
 
 import "fmt"
 
+// これは 2 つの int を受け取って、
+// その合計を int で返す関数です。
 func plus(a int, b int) int {
+	// Go では、明示的な return が必要です。つまり、
+	// 最後に評価した式を自動的には return してくれません。
 	return a + b
 }
 
+// 同じ型の引数が連続する場合は、途中の引数の型名を省略し、
+// 最後の引数にのみ型を宣言することもできます。
 func plusPlus(a, b, c int) int {
 	return a + b + c
 }
 
 func main() {
+	// 期待通り、 name(args) で関数を呼び出せます。
 	res := plus(1, 2)
 	fmt.Println("1+2 =", res)
 
