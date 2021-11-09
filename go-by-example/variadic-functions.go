7553b30ea78daf78aea24d51009157b2ddfa1475
ichisemasashi <ichise.masashi@gmail.com>
2021-11-09 19:54:19 +0900

    add variadic-functions.go

--- go-by-example/variadic-functions.go
+++ go-by-example/variadic-functions.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// これは任意個の int を引数として受け取る関数です。
 func sum(nums ...int) {
 	fmt.Print(nums, " ")
 	total := 0
@@ -12,9 +13,13 @@ func sum(nums ...int) {
 }
 
 func main() {
+	// 可変長引数関数は、通常通り個々の
+	// 引数を渡して呼び出せます。
 	sum(1, 2)
 	sum(1, 2, 3, 4)
 
+	// 複数の引数をすでにスライスでもっている場合は、
+	// func(slice...) のような形で可変長引数関数に渡せます。
 	nums := []int{1, 2, 3, 4}
 	sum(nums...)
 }
