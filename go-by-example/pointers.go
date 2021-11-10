8404e1b3c9c53852e0ad102d58d8fccca7fb4fc0
ichisemasashi <ichise.masashi@gmail.com>
2021-11-10 20:22:32 +0900

    add pointers.go

--- go-by-example/pointers.go
+++ go-by-example/pointers.go
@@ -2,10 +2,21 @@ package main
 
 import "fmt"
 
+// 値と対比しながら、ポインタがどのように動作するかを
+// 2 つの関数 zeroval と zeroptr を使って示します。
+// zeroval は int 型のパラメーターをもつので、引数は
+// 値で渡されます。 zeroval は、呼び出し元の関数とは
+// 別の ival コピーを受け取ります。
 func zeroval(ival int) {
 	ival = 0
 }
 
+// 一方、 zeroptr は *int 型のパラメーターをもつので、
+// int のポインタを受け取ります。関数本体の *iptr は、
+// ポインタを デリファレンス (dereferences) し、
+// ポインタの指すメモリアドレスから現在の値を取得します。
+// ポインタのデリファレンスに値を代入すると、 参照されて
+// いるアドレスにある値が変わります。
 func zeoptr(iptr *int) {
 	*iptr = 0
 }
@@ -17,8 +28,11 @@ func main() {
 	zeroval(i)
 	fmt.Println("zeroval:", i)
 
+	// &i という構文で、 i のメモリアドレス、
+	// すなわち i へのポインタを取得できます。
 	zeoptr(&i)
 	fmt.Println("zeroptr:", i)
 
+	// ポインタは表示することもできます。
 	fmt.Println("pointer:", &i)
 }
