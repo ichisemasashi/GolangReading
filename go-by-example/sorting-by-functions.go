34d5e6316b2e58dc238063081d3e57c2b7918b4c
ichisemasashi <ichise.masashi@gmail.com>
2021-11-17 20:38:32 +0900

    add sorting-by-functions.go

--- go-by-example/sorting-by-functions.go
+++ go-by-example/sorting-by-functions.go
@@ -6,8 +6,8 @@ import (
 )
 
 /*
-   Go でカスタム関数を使ってソートするためには、 対応する型が必要です。
-   ここでは、byLength 型を作りました。これは、 組み込みの []string 型の
+   Go でカスタム関数を使ってソートするためには、対応する型が必要です。
+   ここでは、byLength 型を作りました。これは、組み込みの []string 型の
    ただのエイリアスです。
 */
 type byLength []string
@@ -16,7 +16,7 @@ type byLength []string
    sort パッケージの Sort 関数を使えるように、 sort.Interface
    すなわち Len, Less, Swap 関数を実装します。 Len と Swap は
    どの型でもだいたい同じになり、 Less が実際のカスタムソートのロジックを
-   もちます。 この例では、文字列の長さの昇順でソートしたいので、
+   もちます。この例では、文字列の長さの昇順でソートしたいので、
    len(s[i]) と len(s[j]) を使っています。
 */
 func (s byLength) Len() int {
