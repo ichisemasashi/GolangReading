9de36a970ca1526a0fb770e700fa4fb11dd2c22a
ichisemasashi <ichise.masashi@gmail.com>
2022-02-18 23:24:01 +0900

    No. 22 -10以下または10以上

---
+++ 基礎プロI 100本ノック/初級編/knock22.go
@@ -0,0 +1,15 @@
+// 22 -10以下または10以上
+// 整数値を入力させ、その値が-10以下、または、10以上であればOKと表示するプログラムを作成せよ。
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Print("input number: ")
+	fmt.Scan(&n)
+
+	if (n <= -10) || (10 <= n) {
+		fmt.Println("OK")
+	}
+}
